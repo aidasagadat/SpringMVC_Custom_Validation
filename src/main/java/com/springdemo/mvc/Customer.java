da4312d48567ec7b32eafd75a5abc8ae5f8497ee
Aida Sagadat <aidasagadat355@gmail.com>
2024-05-05 21:12:35 +0500

    add validation rule to Customer class and update customer form

--- src/main/java/com/springdemo/mvc/Customer.java
+++ src/main/java/com/springdemo/mvc/Customer.java
@@ -1,10 +1,9 @@
 package com.springdemo.mvc;
-
+import com.springdemo.mvc.validation.CourseCode;
 import jakarta.validation.constraints.*;
 
+
 public class Customer {
-
-
 
     private String firstName;
 
@@ -22,6 +21,8 @@ public class Customer {
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 characters allowed")
     private String postalCode;
 
+    @CourseCode()
+    private String courseCode;
 
 
     public String getFirstName() {
@@ -54,5 +55,13 @@ public class Customer {
 
     public void setPostalCode(String postalCode) {
         this.postalCode = postalCode;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
     }
 }
